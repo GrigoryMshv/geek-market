9ae6c5e0696548128c0748539135c8526fc0c900
Grigory Moshev <grogory.mshv@gmail.com>
2020-09-25 00:53:30 +0300

    Initial commit

---
+++ src/test/java/ru/geekbrains/geekmarket/GeekMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.geekmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
