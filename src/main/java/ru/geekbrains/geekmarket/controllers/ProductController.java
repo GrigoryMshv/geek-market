38bc3c995c7a4671978b913936422b8b3ed2ee62
Grigory Moshev <grogory.mshv@gmail.com>
2020-10-06 10:22:03 +0300

    Lesson_6

--- src/main/java/ru/geekbrains/geekmarket/controllers/ProductController.java
+++ src/main/java/ru/geekbrains/geekmarket/controllers/ProductController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.geekbrains.geekmarket.entities.Products;
+import ru.geekbrains.geekmarket.exceptions.ResourceNotFoundException;
 import ru.geekbrains.geekmarket.services.ProductService;
+import ru.geekbrains.geekmarket.utilits.ProductFilter;
 
 import java.util.Map;
 
@@ -20,13 +22,22 @@ public class ProductController {
 
    @GetMapping
    public String showAllProducts(Model model, @RequestParam(defaultValue = "1", name = "p") Integer page,
-                                 Map<String, String> params
-                                 ) {
+                                              @RequestParam Map<String, String> params) {
       if (page < 1) {
          page = 1;
       }
-      Page<Products> products = productService.findAll(params, page - 1, PAGE_SIZE);
+      ProductFilter productFilter = new ProductFilter(params);
+      Page<Products> products = productService.findAll(productFilter.getSpec(), page - 1, PAGE_SIZE);
       model.addAttribute("products", products);
+      model.addAttribute("filterDefinition", productFilter.getFilterDefinition());
       return "products";
+   }
+
+   @GetMapping("/edit/{id}")
+   public String showEditForm(@PathVariable Long id, Model model) {
+      Products p = productService.findById(id).orElseThrow(() -> new ResourceNotFoundException("Product with id: " + id + " doesn't exists (for edit)"));
+      model.addAttribute("products", p);
+      return "products";
+
    }
 }
--- src/main/java/ru/geekbrains/geekmarket/services/ProductService.java
+++ src/main/java/ru/geekbrains/geekmarket/services/ProductService.java
@@ -7,9 +7,8 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import ru.geekbrains.geekmarket.entities.Products;
 import ru.geekbrains.geekmarket.repositories.ProductRepository;
-import ru.geekbrains.geekmarket.repositories.specifications.ProductSpecifications;
 
-import java.util.Map;
+import java.util.Optional;
 
 
 @Service
@@ -17,17 +16,15 @@ import java.util.Map;
 public class ProductService {
     private ProductRepository productRepository;
 
-    public Page<Products> findAll(Map<String, String> params, int page, int size) {
-        Specification<Products> spec = Specification.where(null);
-        if(params.containsKey("title") && !params.get("title").isBlank()) {
-            spec = spec.and(ProductSpecifications.titleLike(params.get("title")));
-        }
-        if(params.containsKey("min") && !params.get("min").isBlank()) {
-            spec = spec.and(ProductSpecifications.priceGreaterOrEqualsThan(Float.parseFloat(params.get("min"))));
-        }
-        if(params.containsKey("max") && !params.get("max").isBlank()) {
-            spec = spec.and(ProductSpecifications.priceLesserOrEqualsThan(Float.parseFloat(params.get("max"))));
-        }
+    public Page<Products> findAll(Specification<Products> spec, int page, int size) {
         return productRepository.findAll(spec, PageRequest.of(page, size));
+    }
+
+    public Optional<Products> findById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Products saveOrUpdate(Products products) {
+        return productRepository.save(products);
     }
 }
